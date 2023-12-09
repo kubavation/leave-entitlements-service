851010b35c1f5e5f8b677af45e656bdf2c963bab
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-09 12:08:34 +0100

    init'

---
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/LeaveEntitlementsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.leaveentitlementsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveEntitlementsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveEntitlementsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durys/jakub/leaveentitlementsservice/LeaveEntitlementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.leaveentitlementsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveEntitlementsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
