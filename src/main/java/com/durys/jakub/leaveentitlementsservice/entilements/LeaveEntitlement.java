299db8198d6dba0ed8ed934d31677a7943731ea0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-10 18:13:20 +0100

    validation in value objects

--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/LeaveEntitlement.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/LeaveEntitlement.java
@@ -1,9 +1,17 @@
 package com.durys.jakub.leaveentitlementsservice.entilements;
 
+import java.util.UUID;
+
 public class LeaveEntitlement {
 
     public record Id(AbsenceType absenceType, TenantId tenantId) {
-        
+
+        public Id(String absenceType, UUID tenantId) {
+            this(new AbsenceType(absenceType), new TenantId(tenantId));
+        }
+
     }
+
+
 
 }
--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/TenantId.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/TenantId.java
@@ -1,6 +1,14 @@
 package com.durys.jakub.leaveentitlementsservice.entilements;
 
+import java.util.Objects;
 import java.util.UUID;
 
 record TenantId(UUID value) {
+
+    TenantId {
+        if (Objects.isNull(value)) {
+            throw new RuntimeException("Tenant id cannot be empty");
+        }
+    }
+
 }
