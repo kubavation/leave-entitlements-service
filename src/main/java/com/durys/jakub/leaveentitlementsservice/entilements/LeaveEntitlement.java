3524d1862c886a94eb1f16d84a82146233994908
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-10 17:56:44 +0100

    additional vos + id

--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/LeaveEntitlement.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/LeaveEntitlement.java
@@ -1,4 +1,9 @@
 package com.durys.jakub.leaveentitlementsservice.entilements;
 
 public class LeaveEntitlement {
+
+    public record Id(AbsenceType absenceType, TenantId tenantId) {
+        
+    }
+
 }
---
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/TenantId.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.leaveentitlementsservice.entilements;
+
+import java.util.UUID;
+
+record TenantId(UUID value) {
+}
