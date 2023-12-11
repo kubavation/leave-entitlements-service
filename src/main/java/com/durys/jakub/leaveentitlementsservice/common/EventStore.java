20c471c3281ab908338818d185d5babfa41c7290
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-11 21:06:01 +0100

    change in load method - event store

--- src/main/java/com/durys/jakub/leaveentitlementsservice/common/EventStore.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/common/EventStore.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface EventStore {
-    <T extends AggregateRoot> T load(UUID aggregateId);
+    <T extends AggregateRoot> T load(UUID aggregateId, Class<T> type);
     <T extends AggregateRoot> void save(T aggregate);
     void save(List<Event> events);
     <T extends AggregateRoot> List<Event> loadEvents(UUID aggregateId, Class<T> type);
