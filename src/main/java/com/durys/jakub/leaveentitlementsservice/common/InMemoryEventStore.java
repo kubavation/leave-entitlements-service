816ef6237b2a06c323499c3cf97154d65d48b79f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-12 20:02:15 +0100

    fix in db definition

--- src/main/java/com/durys/jakub/leaveentitlementsservice/common/InMemoryEventStore.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/common/InMemoryEventStore.java
@@ -2,14 +2,13 @@ package com.durys.jakub.leaveentitlementsservice.common;
 
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 class InMemoryEventStore implements EventStore {
 
-    private static final Map<UUID, AggregateRoot> DB = new ConcurrentHashMap<>();
+    private static final Map<UUID, List<Event>> DB = new ConcurrentHashMap<>();
 
 
     @Override
@@ -37,14 +36,7 @@ class InMemoryEventStore implements EventStore {
 
     @Override
     public <T extends AggregateRoot> List<Event> loadEvents(UUID aggregateId, Class<T> type) {
-
-        AggregateRoot aggregateRoot = DB.get(aggregateId);
-
-        if (Objects.isNull(aggregateRoot)) {
-            return Collections.emptyList();
-        }
-
-        return aggregateRoot.events;
+        return DB.get(aggregateId);
     }
 
 
