334a4b2277d21e78570c11338cd5a56cee887a7b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-13 19:43:57 +0100

    changes in serializer - static methods

--- src/main/java/com/durys/jakub/leaveentitlementsservice/common/serialization/Serializer.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/common/serialization/Serializer.java
@@ -1,6 +1,27 @@
 package com.durys.jakub.leaveentitlementsservice.common.serialization;
 
-public interface Serializer {
-    byte[] serialize(Object object);
-    <T> T deserialize(byte[] bytes, Class<T> clazz);
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Serializer {
+
+    private static final ObjectMapper mapper = JsonMapper.builder()
+            .addModule(new JavaTimeModule())
+            .build();
+
+    @SneakyThrows
+    public static byte[] serialize(Object object) {
+        return mapper.writeValueAsBytes(object);
+    }
+
+    @SneakyThrows
+    public static <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        return mapper.readValue(bytes, clazz);
+    }
+
 }
