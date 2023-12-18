707af5f6fdb80af53e9d24b588e6155903182d3e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-18 20:53:40 +0100

    init AbsenceConfiguration

---
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/absence/AbsenceConfiguration.java
@@ -0,0 +1,10 @@
+package com.durys.jakub.leaveentitlementsservice.absence;
+
+public class AbsenceConfiguration {
+
+    public final boolean overdueLeave;
+
+    public AbsenceConfiguration(boolean overdueLeave) {
+        this.overdueLeave = overdueLeave;
+    }
+}
