771d2920e63e77e9302526d04ef5fbfa175fd1ed
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-19 19:53:02 +0100

    initial implementation InMemoryLeaveEntitlementsRepository with ES

--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/infrastructure/InMemoryLeaveEntitlementsRepository.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/infrastructure/InMemoryLeaveEntitlementsRepository.java
@@ -1,22 +1,38 @@
 package com.durys.jakub.leaveentitlementsservice.entilements.infrastructure;
 
+import com.durys.jakub.leaveentitlementsservice.common.serialization.Serializer;
 import com.durys.jakub.leaveentitlementsservice.entilements.domain.LeaveEntitlements;
 import com.durys.jakub.leaveentitlementsservice.entilements.domain.LeaveEntitlementsRepository;
+import com.durys.jakub.leaveentitlementsservice.entilements.domain.events.LeaveEntitlementsEvent;
+import com.durys.jakub.leaveentitlementsservice.es.Event;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryLeaveEntitlementsRepository implements LeaveEntitlementsRepository {
 
-    private static final Map<LeaveEntitlements.Id, LeaveEntitlements> DB = new ConcurrentHashMap<>();
+    private static final Map<LeaveEntitlements.Id, List<Event>> DB = new ConcurrentHashMap<>();
 
     @Override
     public LeaveEntitlements load(LeaveEntitlements.Id id) {
-        return DB.get(id);
+        List<Event> events = DB.get(id);
+
+        List<LeaveEntitlementsEvent> domainEvents = events.stream()
+                .map(event -> Serializer.deserialize(event.getData(), LeaveEntitlementsEvent.class))
+                .toList();
+
+        return LeaveEntitlements.recreate(domainEvents);
     }
 
     @Override
     public void save(LeaveEntitlements entitlements) {
-        DB.put(entitlements.id(), entitlements);
+
+        List<Event> events = entitlements.getEvents()
+                .stream()
+                .map(event -> new Event(event.aggregateId(), event.getClass().getSimpleName(), Serializer.serialize(event)))
+                .toList();
+
+        DB.put(entitlements.id(), events);
     }
 }
