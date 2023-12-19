a7e080db08679e9083391d674a864d2de544dbc3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-19 20:48:40 +0100

    init LeaveEntitlementsApplicationService + tests

---
+++ src/test/java/com/durys/jakub/leaveentitlementsservice/entilements/application/LeaveEntitlementsApplicationServiceTest.java
@@ -0,0 +1,30 @@
+package com.durys.jakub.leaveentitlementsservice.entilements.application;
+
+import com.durys.jakub.leaveentitlementsservice.entilements.domain.LeaveEntitlements;
+import com.durys.jakub.leaveentitlementsservice.entilements.domain.LeaveEntitlementsRepository;
+import com.durys.jakub.leaveentitlementsservice.entilements.domain.command.InitializedLeaveEntitlementsCommand;
+import com.durys.jakub.leaveentitlementsservice.entilements.infrastructure.InMemoryLeaveEntitlementsRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaveEntitlementsApplicationServiceTest {
+
+    private final LeaveEntitlementsRepository repository = new InMemoryLeaveEntitlementsRepository();
+    private final LeaveEntitlementsApplicationService leaveEntitlementsApplicationService = new LeaveEntitlementsApplicationService(repository);
+
+    @Test
+    void shouldInitializeLeaveEntitlements() {
+
+        var command = new InitializedLeaveEntitlementsCommand("W", UUID.randomUUID());
+        var id = new LeaveEntitlements.Id(command.absence(), command.tenantId());
+
+        leaveEntitlementsApplicationService.handle(command);
+
+        LeaveEntitlements entitlements = repository.load(id);
+        assertNotNull(entitlements);
+    }
+
+}
