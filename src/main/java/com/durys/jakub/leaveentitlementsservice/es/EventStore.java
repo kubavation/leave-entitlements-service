bc5371ae56a624b654a19339b006786c7404767d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-21 16:22:23 +0100

    cleaning unused classes

--- src/main/java/com/durys/jakub/leaveentitlementsservice/es/EventStore.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/es/EventStore.java
@@ -1,12 +1,13 @@
 package com.durys.jakub.leaveentitlementsservice.es;
 
+import com.durys.jakub.leaveentitlementsservice.cqrs.DomainEvent;
 import com.durys.jakub.leaveentitlementsservice.ddd.AggregateRoot;
 
 import java.util.List;
 
-public interface EventStore {
-    <T extends AggregateRoot> T load(Object aggregateId, Class<T> type);
-    <T extends AggregateRoot> void save(T aggregate);
+public interface EventStore<T extends AggregateRoot<R>, R extends DomainEvent> {
+    T load(Object aggregateId);
+    void save(T aggregate);
     void saveEvents(Object aggregateId, List<Event> events);
-    <T extends AggregateRoot> List<Event> loadEvents(Object aggregateId, Class<T> type);
+    List<Event> loadEvents(Object aggregateId);
 }
