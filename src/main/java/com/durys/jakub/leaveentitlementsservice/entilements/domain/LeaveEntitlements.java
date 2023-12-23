5744d0063565cc7ff83f59e0ab878df5f2595e41
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-23 19:33:38 +0100

    absenceID

--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/LeaveEntitlements.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/LeaveEntitlements.java
@@ -87,7 +87,7 @@ public class LeaveEntitlements extends AggregateRoot<LeaveEntitlementsEvent> {
                         });
         }
 
-        apply(new AbsenceAppended(identifier, from, to, workingTimeSchedule.numberOfWorkingDays()));
+        apply(new AbsenceAppended(identifier, UUID.randomUUID(), from, to, workingTimeSchedule.numberOfWorkingDays()));
     }
 
     public void withdrawAbsence(LocalDate from, LocalDate to) {
@@ -109,14 +109,12 @@ public class LeaveEntitlements extends AggregateRoot<LeaveEntitlementsEvent> {
 
     private void handle(AbsenceAppended event) {
 
-        UUID absenceId = UUID.randomUUID();
-
         Stream.iterate(event.from(), date -> date.plusDays(1))
                 .limit(ChronoUnit.DAYS.between(event.from(), event.to()) + 1)
                 .forEach(date -> {
                     Entitlement entitlement = findEntitlement(date)
                             .orElseThrow(RuntimeException::new);
-                    entitlement.addAbsence(new Absence(absenceId, date));
+                    entitlement.addAbsence(new Absence(event.absenceId(), date));
                 });
     }
 
--- src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/events/LeaveEntitlementsEvent.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/events/LeaveEntitlementsEvent.java
@@ -4,10 +4,11 @@ import com.durys.jakub.leaveentitlementsservice.cqrs.DomainEvent;
 import com.durys.jakub.leaveentitlementsservice.entilements.domain.LeaveEntitlements;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public sealed interface LeaveEntitlementsEvent extends DomainEvent {
 
-    record AbsenceAppended(LeaveEntitlements.Id id, LocalDate from, LocalDate to, Long days) implements LeaveEntitlementsEvent {
+    record AbsenceAppended(LeaveEntitlements.Id id, UUID absenceId, LocalDate from, LocalDate to, Long days) implements LeaveEntitlementsEvent {
 
         @Override
         public Object aggregateId() {
