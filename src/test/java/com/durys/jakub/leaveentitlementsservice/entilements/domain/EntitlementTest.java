6de782ee4c8312b9c5ff5caa2d65c7d794fc29d4
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-27 21:22:44 +0100

    appending absences - test

--- src/test/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/EntitlementTest.java
+++ src/test/java/com/durys/jakub/leaveentitlementsservice/entilements/domain/EntitlementTest.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.leaveentitlementsservice.entilements.domain;
 
+import com.durys.jakub.leaveentitlementsservice.common.exception.DomainValidationException;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -13,6 +14,31 @@ class EntitlementTest {
     void shouldCreateEntitlement() {
         assertDoesNotThrow(() -> new Entitlement(UUID.randomUUID(),
                 LocalDate.of(2023, 1, 1), LocalDate.of(2023, 12, 31), 26));
+    }
+
+    @Test
+    void shouldAddAbsence() {
+        var absence = new Absence(UUID.randomUUID(), LocalDate.of(2023, 5, 5));
+        Entitlement entitlement = init(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 12, 31));
+
+        entitlement.addAbsence(absence);
+
+        assertFalse(entitlement.getAbsences().isEmpty());
+    }
+
+    @Test
+    void shouldNotAddAbsence_whenAbsenceAlreadyExists() {
+        var absence = new Absence(UUID.randomUUID(), LocalDate.of(2023, 5, 5));
+        Entitlement entitlement = init(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 12, 31));
+        entitlement.addAbsence(new Absence(UUID.randomUUID(), absence.at()));
+
+        DomainValidationException exception = assertThrows(DomainValidationException.class, () -> entitlement.addAbsence(absence));
+
+        assertEquals("Absence already exists at %s".formatted(absence.at()), exception.getMessage());
+    }
+
+    Entitlement init(LocalDate from, LocalDate to) {
+        return new Entitlement(UUID.randomUUID(), from, to, 26);
     }
 
 }
