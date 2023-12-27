9c05c18b7eddb673bbceab1d291a8eba0dda51db
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-27 19:11:10 +0100

    day validation + tests

--- src/main/java/com/durys/jakub/leaveentitlementsservice/workingtime/Day.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/workingtime/Day.java
@@ -1,6 +1,25 @@
 package com.durys.jakub.leaveentitlementsservice.workingtime;
 
+import com.durys.jakub.leaveentitlementsservice.common.exception.DomainValidationException;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public record Day(LocalDate at, BigDecimal hours) {}
+public record Day(LocalDate at, BigDecimal hours) {
+
+    public Day {
+        if (Objects.isNull(at)) {
+           throw new DomainValidationException("Date cannot be empty");
+        }
+
+        if (Objects.isNull(hours)) {
+            throw new DomainValidationException("Hours cannot be empty");
+        }
+
+        if (hours.compareTo(BigDecimal.ZERO) < 0) {
+            throw new DomainValidationException("Invalid hours value");
+        }
+    }
+
+}
---
+++ src/test/java/com/durys/jakub/leaveentitlementsservice/workingtime/DayTest.java
@@ -0,0 +1,41 @@
+package com.durys.jakub.leaveentitlementsservice.workingtime;
+
+import com.durys.jakub.leaveentitlementsservice.common.exception.DomainValidationException;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayTest {
+
+    @Test
+    void shouldCreateDay() {
+        assertDoesNotThrow(() -> new Day(LocalDate.of(2023, 1, 1), BigDecimal.valueOf(8)));
+    }
+
+    @Test
+    void shouldNotCreateDay_whenDateIsEmpty() {
+        DomainValidationException exception
+                = assertThrows(DomainValidationException.class, () -> new Day(null, BigDecimal.valueOf(8)));
+        assertEquals("Date cannot be empty", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotCreateDay_whenHoursAreEmpty() {
+        DomainValidationException exception
+                = assertThrows(DomainValidationException.class, () -> new Day(LocalDate.of(2023, 1, 1), null));
+        assertEquals("Hours cannot be empty", exception.getMessage());
+    }
+
+
+    @Test
+    void shouldNotCreateDay_whenHoursAreLessThanZero() {
+        DomainValidationException exception
+                = assertThrows(DomainValidationException.class, () -> new Day(LocalDate.of(2023, 1, 1), new BigDecimal(-1)));
+        assertEquals("Invalid hours value", exception.getMessage());
+    }
+
+
+}
