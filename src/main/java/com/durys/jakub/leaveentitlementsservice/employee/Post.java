3d6dcdaec9ad770672cd2a6b4edd935d67b5eeba
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-28 21:08:44 +0100

    employee post vo

---
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/employee/Post.java
@@ -0,0 +1,20 @@
+package com.durys.jakub.leaveentitlementsservice.employee;
+
+import com.durys.jakub.leaveentitlementsservice.common.exception.DomainValidationException;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+record Post(BigDecimal value) {
+
+    public Post {
+        if (Objects.isNull(value)) {
+            throw new DomainValidationException("Post value cannot be empty");
+        }
+
+        if (value.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new DomainValidationException("Invalid post value");
+        }
+    }
+
+}
