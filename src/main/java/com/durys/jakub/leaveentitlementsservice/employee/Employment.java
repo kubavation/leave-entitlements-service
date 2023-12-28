f6df8d298cdb8915778669c67f2d005f4dbbadf2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-28 21:11:19 +0100

    renaming

--- src/main/java/com/durys/jakub/leaveentitlementsservice/employee/Employment.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/employee/Employment.java
@@ -3,5 +3,5 @@ package com.durys.jakub.leaveentitlementsservice.employee;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record Employee(UUID id, LocalDate employmentDate, LocalDate dismissalDate) {
+public record Employment(UUID id, LocalDate employmentDate, LocalDate dismissalDate) {
 }
