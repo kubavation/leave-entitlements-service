fd79f5d1763e685c9a1f846a35c94abe0cb9d317
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-02 18:40:45 +0100

    tenantId validation

--- src/main/java/com/durys/jakub/leaveentitlementsservice/employee/domain/TenantId.java
+++ src/main/java/com/durys/jakub/leaveentitlementsservice/employee/domain/TenantId.java
@@ -1,6 +1,15 @@
 package com.durys.jakub.leaveentitlementsservice.employee.domain;
 
+import com.durys.jakub.leaveentitlementsservice.common.exception.DomainValidationException;
+
+import java.util.Objects;
 import java.util.UUID;
 
 public record TenantId(UUID value) {
+
+    public TenantId {
+        if (Objects.isNull(value)) {
+            throw new DomainValidationException("Tenant ID value cannot be empty");
+        }
+    }
 }
